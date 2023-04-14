2d3e74bb0627cef69badfe8936fc69f4695f45f1
Srinivas Andukuri <92281933+andukurisanthosh@users.noreply.github.com>
2023-04-14 16:54:19 +0530

    Add project files.

---
+++ BulkeyBookWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkeyBookWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
